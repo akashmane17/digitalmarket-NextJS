d11e9aec572259218100e4bc8eb7953a9523afef
Akash Mane <akashmane@sovtechnologies.com>
2023-12-16 10:33:37 +0530

    navbar setup

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  
+
+  return <div className="flex gap-4 h-4"></div>;
+};
+
+export default NavItems;
